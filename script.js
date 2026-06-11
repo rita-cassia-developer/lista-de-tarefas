14171e33905b35e7e3069223505872b1aaf4b6c0
Rita de Cássia Calixto <cassiarita0599@gmail.com>
2026-06-11 13:30:24 -0300

    refactor: adiciona botão de remover, editar e limpar

--- script.js
+++ script.js
@@ -1,27 +1,81 @@
+let tarefas = [];
 function adicionarTarefa() {
 
-
     let inputTarefa = document.getElementById("inputTarefa");
-    let tarefa = inputTarefa.value.trim();
-    let listaTarefas = document.getElementById("listaTarefas");
+    const tarefa = inputTarefa.value.trim();
+
     let mensagem = document.getElementById("mensagem");
 
     if (tarefa === "") {
         mensagem.textContent = "Erro: O campo não pode ficar vazio!";
         mensagem.style.color = "#ee2222";
+
     } else {
         mensagem.textContent = "Tarefa adicionada com sucesso!";
-        mensagem.style.color = "#046027"
+        mensagem.style.color = "#046027";
+        tarefas.push(tarefa);
+        inputTarefa.value = "";
 
+        renderizarTarefas();
 
-        let novaTarefa = document.createElement("li");
-        novaTarefa.textContent = tarefa;
-        listaTarefas.appendChild(novaTarefa);
     }
-
-    inputTarefa.value = "";
 }
 
 
 
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas");
+    listaTarefas.innerHTML = "";
 
+    for (let i = 0; i < tarefas.length; i++) {
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefas[i];
+
+            let botaoRemover = document.createElement("button");
+            botaoRemover.className = "remover";
+            botaoRemover.textContent = "Remover";
+            botaoRemover.onclick = () => removerTarefa(i);
+
+            let botaoEditar = document.createElement("button");
+            botaoEditar.className = "editar";
+            botaoEditar.textContent = "Editar";
+            botaoEditar.onclick = () => editarTarefa(i);
+
+            novaTarefa.appendChild(botaoRemover);
+            novaTarefa.appendChild(botaoEditar);
+            listaTarefas.appendChild(novaTarefa);
+
+        }
+
+    
+        let botaoLimpar = document.querySelector("button.botao-lista");
+        if (tarefas.length > 0) {
+            botaoLimpar.style.display = "inline-block";
+    } else {
+            botaoLimpar.style.display = "none";
+    }   
+
+    function removerTarefa(i) {
+        tarefas.splice(i, 1);
+        renderizarTarefas();
+        mensagem.textContent = "Tarefa removida com sucesso!";
+    }
+
+    function editarTarefa(i) {
+        let tarefaEditada = prompt("Edite a tarefa: ");
+
+        if (tarefaEditada.trim() !== "") {
+            tarefas[i] = tarefaEditada;
+            renderizarTarefas();
+        }
+    }
+
+    function limparTarefas() {
+
+        tarefas.length = 0;
+        renderizarTarefas();
+        mensagem.textContent = "Todas as tarefas foram limpas!";
+
+    }
+
+}
